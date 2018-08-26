54078d557469c105f6ffe2cb5b8b078ad3da8218
Victor Emanuel <victorers1@hotmail.com>
2018-08-26 18:59:01 -0300

    Adicionei breve introdução e link para artigo usado como referência

--- Unidade 1/classificadorManchetes.py
+++ Unidade 1/classificadorManchetes.py
@@ -1,21 +1,26 @@
 # -*- coding: utf-8 -*-
 
-import feedparser
+# Inspiração: https://monkeylearn.com/blog/practical-explanation-naive-bayes-classifier/
+# CLASSIFICADOR DE MANCHETES PELO MÉTODO DE NAIVE-BAYES
 
+import feedparser
+# Palavras e caracteres a serem removidas das manchetes
 palRemov = ['"', 'sem', '\'','.', 'só', 'à', 'os', 'com', 'como', 'Há', 'há', 'para','tem', 'têm','uma' , 'uns', 'umas', 'um', 'por', 'das', 'é', 'É', 'se', 'dos', 'Em', 'A', 'a', 'o', 'ao','e','O', 'do', 'da', 'de', 'que', 'muito', 'na', 'no', 'até', 'após', 'são', 'mas', 'mais', 'menos', 'tem', 'pouco', 'nas', 'nos', 'em', ':', ';', ',', '!', '?', '-']
+
 #Obtenção dos Feeds do G1
 G1tec=feedparser.parse('http://pox.globo.com/rss/g1/tecnologia/')
 G1pol=feedparser.parse('http://pox.globo.com/rss/g1/politica')
 G1eco=feedparser.parse('http://pox.globo.com/rss/g1/economia/')
+
 
 def filtra(titulo): # Filtra recebe uma frase (manchete da notícia) e trata-a conforme explicado durante o código
     titulo = titulo.lower() # Remove letras maiúsculas
     lista = titulo.split(' ') # Split separa uma string em várias substrings e guarda numa lista
     for i in range(len(lista)):
         lista[i] = lista[i].replace('\'', '') # Tira todas aspas
-    
     # Retorna uma LISTA de 'palavra' tal que 'palavra' são todos os elementos de 'lista' que 1-Não estão em palRemov; 2-Não contêm digitos
     return [palavra for palavra in lista if ((palavra not in palRemov) and palavra.isalpha())]
+
 
 def naive_bayes(dicio, noticia): # Recebe um dicionário e uma lista com as palavras da notícia. Retorna a probabilidade da notícia estar relacionada com o dicionário
     qtdPal=len(dicio.keys()) # Quantidade de palavras em dicio sem considerar a frequência de cada uma
@@ -30,6 +35,7 @@ def naive_bayes(dicio, noticia): # Recebe um dicionário e uma lista com as pala
             prob = prob*(1/(qtdPal+qtdPalFreq)) # Aplicamos a normalização de Laplace para amostrar com prob. 0
     return prob
 
+
 def contagem(feedNoticia): # Recebe um objeto do tipo 'feed' e cria a lista de palavras e o dicionário de frequências
     dicio = {} # Dicionário a ser preenchido
     for post in feedNoticia.entries: # Para cada notícia recebida
@@ -42,9 +48,9 @@ def contagem(feedNoticia): # Recebe um objeto do tipo 'feed' e cria a lista de p
     return dicio # Retorna o dicionário com todas as frequências de ocorrência incrementadas em uma unidade 1 da real
 
 
-dicTec={} # Dicionário com frequência de cada palavra de tecnologia (dicTec['palavra'] = freq. de ocorrência)
-dicPol={} # Agora com freq. de cada palavra de política
-dicEco={} # E economia
+#dicTec={} # Dicionário com frequência de cada palavra de tecnologia (dicTec['palavra'] = freq. de ocorrência)
+#dicPol={} # Agora com freq. de cada palavra de política
+#dicEco={} # E economia
 
 dicTec = contagem(G1tec) # 
 dicPol = contagem(G1pol) # Criação dos dicionários 
