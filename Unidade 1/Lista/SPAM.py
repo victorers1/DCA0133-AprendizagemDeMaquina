fd905bfac48313354257f98a53c9503a3eb16f31
Victor Emanuel <victorers1@hotmail.com>
2018-09-03 22:27:32 -0300

    Primeira versão finalizada do classificador SPAM

--- Unidade 1/Lista/SPAM.py
+++ Unidade 1/Lista/SPAM.py
@@ -1,28 +1,36 @@
 # -*- coding: utf-8 -*-
 
 # Inspiração: https://monkeylearn.com/blog/practical-explanation-naive-bayes-classifier/
+# Código desenvolvido para Python versão 2.7, versões futuras podem não funcionar
 # CLASSIFICADOR DE SPAM PELO MÉTODO DE NAIVE-BAYES
 
-#from pathlib import Path
+from __future__ import division
+import string
+
 
 # Palavras e caracteres a serem removidas das manchetes
-palRemov = ['"', 'sem', '\'', '.', 'só', 'à', 'os', 'com', 'como', 'Há', 'há', 'para', 'tem', 'têm', 'uma', 'uns',
-            'umas', 'um', 'por', 'das', 'é', 'É', 'se', 'dos', 'Em', 'A', 'a', 'o', 'ao', 'e', 'O', 'do', 'da', 'de',
-            'que', 'muito', 'na', 'no', 'até', 'após', 'são', 'mas', 'mais', 'menos', 'tem', 'pouco', 'nas', 'nos',
-            'em', ':', ';', ',', '!', '?', '-']
+palRemov = ['sem', 'só', 'à', 'os', 'aos', 'com', 'como', 'Há', 'há', 'para', 'tem', 'têm', 'uma', 'uns', 'um', 'umas',
+            'por', 'das', 'as', 'é', 'É', 'se', 'dos', 'desse', 'deste', 'dessa', 'desta', 'desses', 'destes', 'dessas',
+            'destas', 'essas', 'essa', 'esse', 'esses', 'Em', 'ao', 'do', 'da', 'de', 'que', 'na', 'no', 'até', 'após',
+            'são', 'mas', 'mais', 'menos', 'tem', 'pouco', 'nas', 'nos', 'em', 'pela', 'pelo', 'pelas', 'pelos', 'ou',
+            'por']
+for letra in string.ascii_letters:
+    palRemov.append(letra)  # Adiciona todas as letras do alfabeto à 'palRemov'
+pontRemov = '\:;,!?-".\'|/[]{}()*@#><~^+='
 
 
 def filtra(texto):  # Filtra recebe um texto (conteúdo bruto do e-mail) e trata-a conforme explicado durante o código
     texto = texto.lower()  # Remove letras maiúsculas
-
+    texto = texto.replace('\n', ' ')
     lista = texto.split(' ')  # Split separa uma string em várias substrings e guarda numa lista
-    for i in range(len(lista)):  # TODO: Talvez o FOR  seja desnecessário. Verei mais tarde
-        lista[i] = lista[i].replace('\'', '')  # Tira todas aspas, esse caractere tem um tratamento especial pois geralmente vem escrito junto às palavras
+    for i in range(len(lista)):  # Para cada palavra na lista...
+        for ponto in pontRemov:  # Para cada caractere inválido...
+            lista[i] = lista[i].replace(ponto, '') # Tira todos pontos, esses caracteres têm um tratamento especial pois geralmente vêm escritos junto às palavras
     # Retorna uma LISTA de 'palavra' tal que 'palavra' são todos os elementos de 'lista' que 1-Não estão em palRemov; 2-Não contêm digitos
-    return [palavra for palavra in lista if ((palavra not in palRemov) and palavra.isalpha())]
+    return [palavra for palavra in lista if((palavra not in palRemov) and palavra.isalpha())]
 
 
-def naive_bayes(dicio, email):  # Recebe um dicionário e uma lista com as palavras do email. Retorna a probabilidade do email estar relacionado com o dicionário
+def naive_bayes(dicio, novo_email):  # Recebe um dicionário e uma lista com as palavras do email. Retorna a probabilidade do email estar relacionado com o dicionário
     qtdPal = len(dicio.keys())  # Quantidade de palavras em 'dicio' sem considerar a frequência de cada uma
     qtdPalFreq = 0  # Quantidade de palavras em 'dicio' considerando frequências
     prob = 1  # Probabilidade inicial
@@ -30,7 +38,7 @@ def naive_bayes(dicio, email):  # Recebe um dicionário e uma lista com as palav
     for pal in dicio:
         qtdPalFreq = qtdPalFreq + dicio[pal]
 
-    for palavra in email:
+    for palavra in novo_email:
         if palavra in dicio.keys():
             prob = prob * (dicio[palavra] / (qtdPal + qtdPalFreq))  # Cálculo da prob. de uma palavra com freq. não nula
         else:
@@ -38,44 +46,36 @@ def naive_bayes(dicio, email):  # Recebe um dicionário e uma lista com as palav
     return prob
 
 
-def contagem(listEmail):  # Recebe uma lista em que cada elemento é o conteúdo de um email
+def contagem(end_arq):  # Recebe o endereço de um arquivo txt e cria um dicionário com suas palavras
     dicio = {}  # Dicionário a ser preenchido
 
-    for palavra in listEmail:  # Para cada palavra no título da notícia
+    arq = open(end_arq, 'r')
+    str = arq.read()
+    list_email = filtra(str)
+
+    for palavra in list_email:  # Para cada palavra no título da notícia
         if palavra in dicio.keys():  # Se a palvra já é conhecida...
             dicio[palavra] = dicio[palavra] + 1  # Incremente a freq. de ocorrência
         else:  # Se é uma palavra nova...
             dicio[palavra] = 2  # Adicione ao dicionário já com a freq. igual à 2, pois faremos a normalização de Laplace
-
+    arq.close()
     return dicio  # Retorna o dicionário com todas as frequências de ocorrência incrementadas em uma unidade 1 da real
 
-caminhoSPAM = '/home/victor/Área de Trabalho/DCA0133-AprendizagemDeMaquina/Unidade 1/emails/Spam/'
-caminhoNSPAM = '/home/victor/Área de Trabalho/DCA0133-AprendizagemDeMaquina/Unidade 1/emails/NaoSpam/'
 
-listSPAM = []
-listNSPAM = []
+dicSPAM = contagem('C:/Users/victo/OneDrive/UFRN/Apredizagem de Maquina/Unidade 1/Lista/emails/spam.txt')  # Criação dos dicionários
+dicNSPAM = contagem('C:/Users/victo/OneDrive/UFRN/Apredizagem de Maquina/Unidade 1/Lista/emails/naospam.txt')  # Serão lidos arquivos de texto com o conteúdo de emails
 
-for i in range(1, 24, 1):
-    arq = open(caminhoSPAM+str(i)+'.txt', 'r')
-    listSPAM.append(arq.read() + ' ')
-    arq.close()
-
-for i in range(1, 27, 1):
-    arq = open(caminhoNSPAM+str(i)+'.txt', 'r')
-    listNSPAM.append(arq.read() + ' ')
-    arq.close()
-
-dicSPAM = contagem()   # Criação dos dicionários
-dicNSPAM = contagem()  # Serão lidos arquivos de texto com o conteúdo de emails
 
 while True:
     # Rebece um exemplo de email para classificar, filtra certas palavras e guarda numa lista
-    email = filtra(input('Digite o conteúdo de um email: '))
+    email = filtra(input('Digite o conteúdo de um email entre aspas: '))  # Entre aspas porque a versão usada é a 2.7
 
     probSPAM = naive_bayes(dicSPAM, email)  # Calcula probabilidade de ser SPAM
-
     probNSPAM = naive_bayes(dicNSPAM, email)  # Calcula probabilidade de ser NÃO ser SPAM
     # A soma das probabilidades não precisa ser igual a 1
 
-    print('prob. spam: ' + str(probSPAM))  #
+    print('prob. spam: '     + str(probSPAM))  #
     print('prob. não spam: ' + str(probNSPAM))  # Os valores geralmente são bem pequenos
+
+    dicConclusao = {probSPAM: 'SPAM', probNSPAM: 'NÃO SPAM'}
+    print('Conclusão: '+dicConclusao[max(dicConclusao.keys())])
--- Unidade 1/classificadorManchetes.py
+++ Unidade 1/classificadorManchetes.py
@@ -5,7 +5,11 @@
 
 import feedparser
 # Palavras e caracteres a serem removidas das manchetes
-palRemov = ['"', 'sem', '\'','.', 'só', 'à', 'os', 'com', 'como', 'Há', 'há', 'para','tem', 'têm','uma' , 'uns', 'umas', 'um', 'por', 'das', 'é', 'É', 'se', 'dos', 'Em', 'A', 'a', 'o', 'ao','e','O', 'do', 'da', 'de', 'que', 'muito', 'na', 'no', 'até', 'após', 'são', 'mas', 'mais', 'menos', 'tem', 'pouco', 'nas', 'nos', 'em', ':', ';', ',', '!', '?', '-']
+palRemov = ['"', 'sem', '\'', '.', 'só', 'à', 'os', 'com', 'como', 'Há', 'há', 'para', 'tem', 'têm', 'uma', 'uns',
+            'umas', 'um', 'por', 'das', 'é', 'É', 'se', 'dos', 'Em', 'A', 'a', 'o', 'ao', 'e', 'O', 'do', 'da', 'de',
+            'que', 'muito', 'na', 'no', 'até', 'após', 'são', 'mas', 'mais', 'menos', 'tem', 'pouco', 'nas', 'nos',
+            'em', ':', ';', ',', '!', '?', '-', 'b', 'c', 'd', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'w,' 'z']
 
 #Obtenção dos Feeds do G1
 G1tec=feedparser.parse('http://pox.globo.com/rss/g1/tecnologia/')
@@ -13,48 +17,48 @@ G1pol=feedparser.parse('http://pox.globo.com/rss/g1/politica')
 G1eco=feedparser.parse('http://pox.globo.com/rss/g1/economia/')
 
 
-def filtra(titulo): # Filtra recebe uma frase (manchete da notícia) e trata-a conforme explicado durante o código
-    titulo = titulo.lower() # Remove letras maiúsculas
-    lista = titulo.split(' ') # Split separa uma string em várias substrings e guarda numa lista
+def filtra(titulo):  # Filtra recebe uma frase (manchete da notícia) e trata-a conforme explicado durante o código
+    titulo = titulo.lower()  # Remove letras maiúsculas
+    lista = titulo.split(' ')  # Split separa uma string em várias substrings e guarda numa lista
     for i in range(len(lista)):
-        lista[i] = lista[i].replace('\'', '') # Tira todas aspas
+        lista[i] = lista[i].replace('\'', '')  # Tira aspas, esse caractere tem um tratamento especial pois geralmente vem escrito junto às palavras
     # Retorna uma LISTA de 'palavra' tal que 'palavra' são todos os elementos de 'lista' que 1-Não estão em palRemov; 2-Não contêm digitos
     return [palavra for palavra in lista if ((palavra not in palRemov) and palavra.isalpha())]
 
 
-def naive_bayes(dicio, noticia): # Recebe um dicionário e uma lista com as palavras da notícia. Retorna a probabilidade da notícia estar relacionada com o dicionário
-    qtdPal=len(dicio.keys()) # Quantidade de palavras em dicio sem considerar a frequência de cada uma
-    qtdPalFreq=0 # Quantidade de palavras em dicio considerando frequências
+def naive_bayes(dicio, noticia):  # Recebe um dicionário e uma lista com as palavras da notícia. Retorna a probabilidade da notícia estar relacionada com o dicionário
+    qtdPal=len(dicio.keys())  # Quantidade de palavras em dicio sem considerar a frequência de cada uma
+    qtdPalFreq=0  # Quantidade de palavras em dicio considerando frequências
     for pal in dicio:
         qtdPalFreq = qtdPalFreq+dicio[pal]
-    prob=1 # Probabilidade inicial
+    prob=1  # Probabilidade inicial
     for palavra in noticia:
         if palavra in dicio.keys():
-            prob = prob*(dicio[palavra]/(qtdPal+qtdPalFreq)) # Cálculo da prob. de uma palavra com freq. não nula
+            prob = prob*(dicio[palavra]/(qtdPal+qtdPalFreq))  # Cálculo da prob. de uma palavra com freq. não nula
         else:
-            prob = prob*(1/(qtdPal+qtdPalFreq)) # Aplicamos a normalização de Laplace para amostrar com prob. 0
+            prob = prob*(1/(qtdPal+qtdPalFreq))  # Aplicamos a normalização de Laplace para amostrar com prob. 0
     return prob
 
 
-def contagem(feedNoticia): # Recebe um objeto do tipo 'feed' e cria a lista de palavras e o dicionário de frequências
-    dicio = {} # Dicionário a ser preenchido
-    for post in feedNoticia.entries: # Para cada notícia recebida
-        listaPalavra = filtra(post.title) # Cria uma lista de palavras filtradas do título da notícia
-        for palavra in listaPalavra: # Para cada palavra no título da notícia
-            if palavra in dicio.keys(): # Se a palvra já é conhecida...
-                dicio[palavra] = dicio[palavra]+1 # Incremente a freq. de ocorrência
-            else: # Se é uma palavra nova...
-                dicio[palavra] = 2 # Adicione ao dicionário já com a freq. igual à 2, pois faremos a normalização de Laplace
-    return dicio # Retorna o dicionário com todas as frequências de ocorrência incrementadas em uma unidade 1 da real
+def contagem(feedNoticia):  # Recebe um objeto do tipo 'feed' e cria a lista de palavras e o dicionário de frequências
+    dicio = {}  # Dicionário a ser preenchido
+    for post in feedNoticia.entries:  # Para cada notícia recebida
+        listaPalavra = filtra(post.title)  # Cria uma lista de palavras filtradas do título da notícia
+        for palavra in listaPalavra:  # Para cada palavra no título da notícia
+            if palavra in dicio.keys():  # Se a palvra já é conhecida...
+                dicio[palavra] = dicio[palavra]+1  # Incremente a freq. de ocorrência
+            else:  # Se é uma palavra nova...
+                dicio[palavra] = 2  # Adicione ao dicionário já com a freq. igual à 2, pois faremos a normalização de Laplace
+    return dicio  # Retorna o dicionário com todas as frequências de ocorrência incrementadas em uma unidade 1 da real
 
 
 #dicTec={} # Dicionário com frequência de cada palavra de tecnologia (dicTec['palavra'] = freq. de ocorrência)
 #dicPol={} # Agora com freq. de cada palavra de política
 #dicEco={} # E economia
 
-dicTec = contagem(G1tec) # 
-dicPol = contagem(G1pol) # Criação dos dicionários 
-dicEco = contagem(G1eco) #
+dicTec = contagem(G1tec)  #
+dicPol = contagem(G1pol)  # Criação dos dicionários
+dicEco = contagem(G1eco)  #
 
 while True:
     # Rebece nova notícia para classificar, filtra certas palavras e guarda numa lista
@@ -68,9 +72,9 @@ while True:
     probEco = naive_bayes(dicEco, listNoticia)
     # A SOMA DAS PROBABILIDADES NÃO É IGUAL À 1
     
-    print('proTec: ' + str(probTec)) #
-    print('proPol: ' + str(probPol)) # Os valores geralmente são bem pequenos
-    print('proEco: ' + str(probEco)) #
+    print('proTec: ' + str(probTec))  #
+    print('proPol: ' + str(probPol))  # Os valores geralmente são bem pequenos
+    print('proEco: ' + str(probEco))  #
     
     dicio = {probTec:'Tecnologia', probPol:'Política', probEco:'Economia'}
-    print(dicio[max(dicio.keys())]) # Imprime o assunto cuja probabilidade é máxima
+    print(dicio[max(dicio.keys())])  # Imprime o assunto cuja probabilidade é máxima
